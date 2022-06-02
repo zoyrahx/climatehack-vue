bd39487760c5a22a245cd33879c58e36c42ee600
zmaranon <maranon.zy@gmail.com>
2022-06-02 19:45:50 +0900

    Initial commit of vue files
    Initial commit of vue files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ClimateHack msg="Climate Hackathon 2022"/>
+  </div>
+</template>
+
+<script>
+import ClimateHack from './components/ClimateHack.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ClimateHack
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ClimateHack.vue
@@ -0,0 +1,58 @@
+
+<template>
+  <div class="climateHack">
+    <h1>{{ msg }}</h1>
+    <h4>Team Epsilon</h4>
+
+    <p>Sample page</p>
+    <iframe width="560" height="315" src="https://public.tableau.com/views/MeasuringReactionsReactionAnalytics/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link&:embed=yes&:tabs=yes&:toolbar=yes"></iframe>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ClimateHack',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  mounted(){
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/"
+        );
+        this.data = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
