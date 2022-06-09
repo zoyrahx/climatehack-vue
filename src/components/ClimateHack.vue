acc5082b983b0dc7c0b83d76ad80cadd92400b14
zmaranon <71584825+zoyrahx@users.noreply.github.com>
2022-06-09 12:38:06 +0900

    Updated iframe

--- src/components/ClimateHack.vue
+++ src/components/ClimateHack.vue
@@ -5,7 +5,7 @@
     <h4>AVERA</h4>
 
     <p>Automated Vessel Emission and Route Analysis</p>
-    <iframe width="560" height="315" src="https://app.powerbi.com/groups/me/reports/add09b10-90d7-47ac-879a-d3e3f551bff5/ReportSection"></iframe>
+    <iframe title="AVERA Dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=add09b10-90d7-47ac-879a-d3e3f551bff5&autoAuth=true&ctid=e93c0c9e-286e-4c79-b4cd-be2a9d8ccf16&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWVhc3QtYXNpYS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
   </div>
 </template>
 
